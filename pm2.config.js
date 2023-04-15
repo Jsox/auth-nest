a2139f500fd1f97feafc6409be542bbabd6d39d2
Jsox <mrjohn73@yandex.ru>
2023-04-16 00:05:48 +0300

    Update pm2.config.js

--- pm2.config.js
+++ pm2.config.js
@@ -11,7 +11,7 @@ module.exports = {
             user: 'jsix-auth',
             host: ['192.168.0.13', '192.168.0.14', '192.168.0.15'],
             ref: 'origin/master',
-            repo: 'git@github.com:Username/repository.git',
+            repo: 'git@github.com:Jsox/auth-nest.git',
             path: '/home/jsix-auth/htdocs/auth.jsix.ru',
             'post-deploy': 'npm install',
         },
